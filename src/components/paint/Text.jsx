ddb2b30d09a39c4113683a8330c1b49a73bd8dc8
lucas.singh@bizitglobal.com <lucas.sing@bizitglobal.com>
2025-01-19 11:51:16 -0300

    cambios

---
+++ src/components/paint/Text.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Text() {
+    return (
+        <div className="absolute flex flex-col text-[5.5vw] uppercase w-[70vw] items-start leading-tight">
+            <h2 className="self-end font-light lowercase">Front End Developer</h2>
+            <h2 className='lowercase font-light'>Based in Argentina</h2>
+            <h2 className="self-end font-light lowercase">Portfolio@2025</h2>
+        </div>
+    )
+}
--- src/components/scene/Index.jsx
+++ src/components/scene/Index.jsx
@@ -2,9 +2,8 @@
 import { Canvas } from '@react-three/fiber'
 import Model from './Model';
 import { Environment } from '@react-three/drei'
-import Section from '../section';
-import Paragraph2 from '../paragraph/paragraph2';
-import Paragraph3 from '../paragraph/paragraph3';
+import SceneReveal from '../paint/scenereveal';
+import Text from '../paint/Text';
 
 export default function Index() {
 
@@ -16,67 +15,10 @@ export default function Index() {
                     <directionalLight intensity={2} position={[0, 2, 3]} />
                     <Environment preset="city" />
                 </Canvas>
-                <Section bgColor='#1c1c1c'>
-                   <Paragraph2 paragraph='I am a passionate web developer with a strong focus on creating dynamic, 
-                        scalable, and high-quality applications. I am driven by the desire to contribute my skills to impactful projects, 
-                        delivering efficient and innovative solutions that meet users needs.' 
-                    />
-                </Section>
-
-                <Section bgColor='yellow'>
-                    <div className='flex flex-col'>
-                        <div className='bg-slate-600'>
-                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam velit voluptatibus tempora delectus facere accusamus dolorem beatae voluptas, numquam officia repellat similique tempore rem asperiores iusto. Ab nisi voluptatem ipsam.
-
-
-                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit harum nam sapiente suscipit, iste consequuntur voluptatibus, nisi quisquam qui nobis fugiat dignissimos quasi reprehenderit animi minima quaerat hic officia ipsam!
-
-
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi perferendis temporibus, provident animi, accusantium fugit ea neque exercitationem sequi enim, sunt nam maiores recusandae deserunt nulla ab reprehenderit magnam laborum?
-
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia asperiores id, et voluptatem itaque magni rerum possimus similique nostrum voluptas, sapiente sunt ex quibusdam deserunt iusto debitis voluptates, porro est.
-
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, tempora! Amet repellat exercitationem dignissimos tenetur debitis eligendi molestias, dolorum ipsum optio saepe quisquam obcaecati sunt commodi voluptatum assumenda quibusdam asperiores.
-
-                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur ad aliquid ipsam, rem hic quia eos temporibus nihil nobis dolorem debitis nisi! Eos quia harum doloremque asperiores, sint distinctio quod.
-                        </div>
-                        <div className='bg-lime-600 mt-3'>
-                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam velit voluptatibus tempora delectus facere accusamus dolorem beatae voluptas, numquam officia repellat similique tempore rem asperiores iusto. Ab nisi voluptatem ipsam.
-
-
-                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit harum nam sapiente suscipit, iste consequuntur voluptatibus, nisi quisquam qui nobis fugiat dignissimos quasi reprehenderit animi minima quaerat hic officia ipsam!
-
-
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi perferendis temporibus, provident animi, accusantium fugit ea neque exercitationem sequi enim, sunt nam maiores recusandae deserunt nulla ab reprehenderit magnam laborum?
-
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia asperiores id, et voluptatem itaque magni rerum possimus similique nostrum voluptas, sapiente sunt ex quibusdam deserunt iusto debitis voluptates, porro est.
-
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, tempora! Amet repellat exercitationem dignissimos tenetur debitis eligendi molestias, dolorum ipsum optio saepe quisquam obcaecati sunt commodi voluptatum assumenda quibusdam asperiores.
-
-                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur ad aliquid ipsam, rem hic quia eos temporibus nihil nobis dolorem debitis nisi! Eos quia harum doloremque asperiores, sint distinctio quod.
-                        </div>
-                        <div className='bg-blue-600 mt-3'>
-                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam velit voluptatibus tempora delectus facere accusamus dolorem beatae voluptas, numquam officia repellat similique tempore rem asperiores iusto. Ab nisi voluptatem ipsam.
-
-
-                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit harum nam sapiente suscipit, iste consequuntur voluptatibus, nisi quisquam qui nobis fugiat dignissimos quasi reprehenderit animi minima quaerat hic officia ipsam!
-
-
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi perferendis temporibus, provident animi, accusantium fugit ea neque exercitationem sequi enim, sunt nam maiores recusandae deserunt nulla ab reprehenderit magnam laborum?
-
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia asperiores id, et voluptatem itaque magni rerum possimus similique nostrum voluptas, sapiente sunt ex quibusdam deserunt iusto debitis voluptates, porro est.
-
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, tempora! Amet repellat exercitationem dignissimos tenetur debitis eligendi molestias, dolorum ipsum optio saepe quisquam obcaecati sunt commodi voluptatum assumenda quibusdam asperiores.
-
-                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur ad aliquid ipsam, rem hic quia eos temporibus nihil nobis dolorem debitis nisi! Eos quia harum doloremque asperiores, sint distinctio quod.
-                        </div>
-                    </div>
-                </Section>
-                <Section bgColor='#1c1c1c'>
-                    <Paragraph3 paragraph='I am a passionate web developer with a strong focus on creating dynamic, 
-                        scalable, and high-quality applications. I am driven by the desire to contribute my skills to impactful projects, 
-                        delivering efficient and innovative solutions that meet users needs.' />
-                </Section>
+                <div className="flex w-full h-screen items-center justify-center">
+                    <Text />
+                    <SceneReveal />
+                </div>
             </div>
         </>
     );
